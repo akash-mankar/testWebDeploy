e3535a2684e514410585bf97aed44d1ffa98dd04
chin <d.chinmaya@utexas.edu>
2013-09-19 00:35:42 -0500

    first file change

--- GuestBook/src/guestbook/GuestBookServlet.java
+++ GuestBook/src/guestbook/GuestBookServlet.java
@@ -6,6 +6,6 @@ import javax.servlet.http.*;
 public class GuestBookServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		resp.setContentType("text/plain");
-		resp.getWriter().println("Akash Chinmay first deployment");
+		resp.getWriter().println("Akash Chinmay 2nd deployment");
 	}
 }
