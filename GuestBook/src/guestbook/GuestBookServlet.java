c15cc6155fcb84970dd3816e2bd61c60fe2888f5
chin <d.chinmaya@utexas.edu>
2013-09-20 22:54:59 -0500

    another commit

--- GuestBook/src/guestbook/GuestBookServlet.java
+++ GuestBook/src/guestbook/GuestBookServlet.java
@@ -6,6 +6,6 @@ import javax.servlet.http.*;
 public class GuestBookServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		resp.setContentType("text/plain");
-		resp.getWriter().println("Akash Chinmay 4th deployment");
+		resp.getWriter().println("Akash-Chinmay 4th deployment");
 	}
 }
